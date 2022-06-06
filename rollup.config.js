8e0c2289f63a34908647a25f358229cbed8c1d94
Konstantin Barabanov <criitch@yandex.ru>
2022-06-06 16:59:55 +0300

    fix: убрал ненужные зависимости; поправил имена; поправил rollup конфиг

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,37 @@
-import dts from 'rollup-plugin-dts'
-import esbuild from 'rollup-plugin-esbuild'
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import external from 'rollup-plugin-peer-deps-external';
+import dts from 'rollup-plugin-dts';
 
-const name = require('./package.json').main.replace(/\.js$/, '')
+const packageJson = require('./package.json');
 
-const bundle = config => ({
-  ...config,
-  input: 'src/index.ts',
-  external: id => !/^[./]/.test(id),
-})
-
-export default [
-  bundle({
-    plugins: [esbuild()],
+export default [{
+    input: 'src/index.ts',
     output: [
-      {
-        file: `${name}.js`,
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: `${name}.mjs`,
-        format: 'es',
-        sourcemap: true,
-      },
+        {
+            file: packageJson.main,
+            format: 'cjs',
+            sourcemap: true,
+            name: 'react-lib'
+        },
+        {
+            file: packageJson.module,
+            format: 'esm',
+            sourcemap: true
+        }
     ],
-  }),
-  bundle({
-    plugins: [dts()],
-    output: {
-      file: `${name}.d.ts`,
-      format: 'es',
-    },
-  }),
+    plugins: [
+        external(),
+        resolve(),
+        commonjs(),
+        typescript({ tsconfig: './tsconfig.json' }),
+    ]
+},
+{
+  input: 'src/index.ts',
+  output: [{ file: packageJson.typings, format: "esm" }],
+  external: [/\.css$/],
+  plugins: [dts()],
+}
 ]
