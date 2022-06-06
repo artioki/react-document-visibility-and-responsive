5525d859c384ddf70354e2825ab71cc14aa81bbf
artioki <kmm.19@uni-dubna.ru>
2022-06-06 16:48:39 +0300

    delete src copy

--- src copy/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './react-responsive';
-export * from './react-visibility';
--- src copy/react-responsive/index.ts
+++
@@ -1,3 +0,0 @@
-
-export {default as MediaQuery} from './MediaQuery';
-export {default as useMediaQuery} from './UseMediaQuery';
